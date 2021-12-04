c7fcaccc5b831428eaa675a3bb5fe865352ef52d
Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>
2021-12-04 15:21:15 +0700

    user: Add Map and Marker for each Laundry Location around User
    Signed-off-by: Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>

--- index.php
+++ index.php
@@ -39,6 +39,15 @@ if(!isset($_COOKIE['user_email'])) {
         integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
     </script>
 
+    <script type="text/javascript" src="js/script.js"></script>
+    
+    <style>
+        #allPositions, #userPosition {
+            display: none;
+        }
+    </style>
+
+
 </head>
 
 <body>
@@ -68,7 +77,103 @@ if(!isset($_COOKIE['user_email'])) {
         <?php endforeach;?>
     </div>
 
+    <?php 
+    $allPositionLaundry = query("SELECT * FROM tb_laundry");
+    $allPositionLaundry = json_encode($allPositionLaundry);
 
+    $userEmail = $_COOKIE['user_email'];
+    $userPosition = query("SELECT latitude,longitude,alamat FROM tb_user WHERE email = '$userEmail'");
+    $userPosition = json_encode($userPosition);
+
+    echo '<div id="userPosition">' . $userPosition . '</div>';
+    echo '<div id="allPositions">' . $allPositionLaundry . '</div>';
+    ?>
+
+    <div id="map_canvas" style="width: 50%; height: 400px"></div>
+
+    <script>
+        //user icon
+        const myLocationIcon = "https://img.icons8.com/ios-filled/50/000000/men-age-group-4.png";
+
+        //get user position
+        var userPos = JSON.parse(document.getElementById('userPosition').innerHTML);
+        var latitude = parseFloat(userPos[0].latitude);
+        var longitude = parseFloat(userPos[0].longitude);
+        var alamat = userPos[0].alamat;
+
+        function initMap() {
+            //initialize user position
+            var map = new google.maps.Map(document.getElementById("map_canvas"), {
+                zoom: 14,
+                center: {lat: latitude, lng: longitude},
+            });
+
+            //Add message box user
+            var infoBoxUser = new google.maps.InfoWindow;
+            var contentUser = document.createElement('div');
+            var bold = document.createElement('b');
+            var paragraf = document.createElement('p');
+            bold.textContent = 'Lokasi anda'
+            paragraf.textContent = alamat;
+            contentUser.style.maxWidth = '150px';
+            contentUser.style.maxHeight = '100%';
+            contentUser.appendChild(bold);
+            contentUser.appendChild(paragraf);
+
+            //Add marker on user position
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(latitude, longitude),
+                icon: myLocationIcon,
+                map: map
+            })
+
+            //Add click event on user marker
+            marker.addListener('click', function(){
+                infoBoxUser.setContent(contentUser);
+                infoBoxUser.open(map, marker);
+            })
+
+
+            //load all Laundry Location
+            var allPositionLaundry = JSON.parse(document.getElementById('allPositions').innerHTML);
+            loadAllPositions(allPositionLaundry, map);
+        }
+
+        function loadAllPositions(data, map) {
+            var infoBox = new google.maps.InfoWindow;
+            Array.prototype.forEach.call(data, function(data){
+                //Add message box laundry
+                var contentBoxLaundry = document.createElement('div');
+                var bold = document.createElement('b');
+                var paragraf = document.createElement('p');
+                var linkDetail = document.createElement('a');
+                bold.textContent = data.nama_laundry;
+                paragraf.textContent = data.alamat;
+                linkDetail.href = 'http://localhost/FP-PWL/detail_laundry.php?id=' + parseInt(data.id_laundry);
+                linkDetail.textContent = 'Cek Laundry'
+                contentBoxLaundry.style.maxWidth = '150px';
+                contentBoxLaundry.style.maxHeight = '100%';
+                contentBoxLaundry.appendChild(bold);
+                contentBoxLaundry.appendChild(paragraf);
+                contentBoxLaundry.appendChild(linkDetail);
+
+                //Add marker on laundry position
+                var vMarker = new google.maps.Marker({
+                    position: new google.maps.LatLng(data.latitude, data.longitude),
+                    map: map
+                });
+
+                //Add click event on marker laundry
+                vMarker.addListener('click', function(){
+                    infoBox.setContent(contentBoxLaundry);
+                    infoBox.open(map, vMarker);
+                })
+            }) 
+        }
+
+    </script>
+
+    <script async type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD7So2OFuOg3mJdwU2h2lkoFz19E5GGag8&callback=initMap"></script>
 
 </body>
 
